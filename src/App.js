e51c0bb52d2a5a7aded63120e03cdd672f806a8c
Nogal1 <mcclendon.logan1234@gmail.com>
2024-09-15 18:44:15 -0700

    refactored App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import JokeList from "./JokeList";
 
 /** App component. Renders list of jokes. */
 
-class App extends Component {
-  render() {
+function App() {
+  render() 
     return (
       <div className="App">
         <JokeList />
       </div>
     );
-  }
 }
 
 export default App;
