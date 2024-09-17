5d121547e3327e0033cf7d0fe0b513ac8ddc1a40
Nogal1 <mcclendon.logan1234@gmail.com>
2024-09-16 19:47:55 -0700

    Joke.js refactored

--- src/Joke.js
+++ src/Joke.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
 import "./Joke.css";
 
-/** A single joke, along with vote up/down buttons. */
+/** A single joke, along with vote up/down buttons. 
+ * Renders a joke, vote buttons, and the current vote count.
+ * 
+ * @param {string} text - The joke text.
+ * @param {number} votes - The current vote count.
+ * @param {function} upvote - Function to handle upvoting the joke.
+ * @param {function} downvote - Function to handle downvoting the joke.
+*/
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
 
-    return (
-      <div className="Joke">
-        <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
-            <i className="fas fa-thumbs-up" />
-          </button>
+function Joke({ text, votes, upvote, downvote }) {
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={upvote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
 
-          <button onClick={evt => vote(id, -1)}>
-            <i className="fas fa-thumbs-down" />
-          </button>
-
-          {votes}
-        </div>
-
-        <div className="Joke-text">{text}</div>
+        {votes}
       </div>
-    );
-  }
+
+      <div className="Joke-text">{text}</div>
+    </div>
+  );
 }
 
 export default Joke;
